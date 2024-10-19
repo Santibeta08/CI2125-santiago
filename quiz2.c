75cac056d3243fa210ba98c494eeb35c464e6f23
santibeta <21-10071@usb.ve>
2024-10-18 21:03:08 -0400

    primer commit

---
+++ quiz2.c
@@ -0,0 +1,29 @@
+/***********************
+*
+* EXPRESIONES DIÁDICAS 1
+*
+**********************/
+    #include <stdio.h>
+int main() {
+    
+    int x,y;
+    
+    for (;;) {
+    
+    printf(";Introduzca el valor de x = ");
+    scanf("%d",&x);
+    printf("Introduzca el valor de y = ");
+    scanf("%d",&y);
+    
+    printf("\n%d + %d = %d\n\n",x,y,x+y);
+    
+    }
+
+    return 0;
+
+}
+/*
+*
+* printf(&quot;\n%d || %d = %d\n&quot;,x,y,x||y);
+*
+*/
